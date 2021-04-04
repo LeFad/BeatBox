cca9ad6dd3fc2b54f6cbd1a0175451cd4d94d1ab
LeFad <leronmarkevich@gmail.com>
2021-04-04 20:37:10 +0300

    SoundPool

--- app/src/main/java/com/valeria/beatbox/Sound.kt
+++ app/src/main/java/com/valeria/beatbox/Sound.kt
@@ -2,6 +2,6 @@ package com.valeria.beatbox
 
 private const val WAV = ".wav"
 
-class Sound(val assetPath: String) { //для генерации удобочитаемого имени звука
+class Sound(val assetPath: String, var soundId: Int? = null) { //для генерации удобочитаемого имени звука
     val name = assetPath.split("/").last().removeSuffix(WAV)
 }
--- app/src/main/java/com/valeria/beatbox/SoundViewModel.kt
+++ app/src/main/java/com/valeria/beatbox/SoundViewModel.kt
@@ -1,11 +1,16 @@
 package com.valeria.beatbox
 
-class SoundViewModel {
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class SoundViewModel: BaseObservable() {
+
     var sound: Sound? = null
     set(sound) {
         field = sound
+        notifyChange()
     }
-
+    @get:Bindable
     val title: String?
-    get() = sound?.name
+        get() = sound?.name
 }
